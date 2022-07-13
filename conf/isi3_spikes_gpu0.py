e627b15d6c9111036a27d3e7eeff20dfc3072f4c
Felix B. Kern <kernfel+github@gmail.com>
2022-07-13 12:40:12 +0900

    File-based parametrisation for isi_genn

---
+++ conf/isi3_spikes_gpu0.py
@@ -0,0 +1,14 @@
+from .params import params
+
+gpuid = 0
+start_at = dict(net=0, isi=100, STD=0, TA=0, templ=1)
+
+N_networks = 30
+N_templates = 3
+ISIs = (100, 500, 1000, 2000)
+fbase = '/data/felix/culture/isi3_'
+fname = fbase + 'net{net}_isi{isi}_STD{STD}_TA{TA}_templ{templ}.h5'
+netfile = fbase + 'net{net}.h5'
+
+stimuli = {key: j for j, key in enumerate('ABCDE')}
+pairings=(('A','B'), ('C','E'))
--- isi_genn.py
+++ isi_genn.py
@@ -1,6 +1,6 @@
 import time
-import multiprocessing as mp
-import matplotlib.pyplot as plt
+import sys
+import importlib
 import deepdish as dd
 from brian2.only import *
 import brian2genn
@@ -11,156 +11,75 @@ import spatial, model, inputs, readout
 
 from util import brian_cleanup
 
+cfg = importlib.import_module('.'.join(sys.argv[1].split('.')[0].split('/')))
 
-gpuid = 1
-working_dir = f'GPU{gpuid}'
+working_dir = f'GPU{cfg.gpuid}'
 rng = np.random.default_rng()
 device_args = dict(directory=working_dir)
 set_device('genn', **device_args)
 prefs.devices.genn.cuda_backend.device_select='MANUAL'
-prefs.devices.genn.cuda_backend.manual_device=gpuid
+prefs.devices.genn.cuda_backend.manual_device=cfg.gpuid
 
 
-N = 1000
-inhibitory_ratio = .2
-params = {
-    # Simulation
-    'dt': 1*ms,
-    
-    # Population size
-    'N': N,
-    'N_exc': int((1-inhibitory_ratio) * N),
-    'N_inh': int(inhibitory_ratio * N),
-
-    # Membrane
-    'v_rest': -60*mV,
-    'v_reset': -74*mV,
-    'v_threshold': -54*mV,
-    'voltage_init': 'v_rest',
-    
-    'tau_mem': 30*ms,
-    'refractory_exc': 3*ms,
-    'refractory_inh': 2*ms,
-
-    # Threshold adaptation - Exc
-    'th_tau': 1*second,
-    'th_ampl': 1*mV,
-
-    # Short-term plasticity - Exc
-    'tau_rec': 150*msecond,
-    'U': 0.4,
-
-    # Synapse dynamics
-    'E_exc': 0*mV,
-    'tau_ampa': 2*msecond,
-    'E_inh': -100*mV,
-    'tau_gaba': 4*msecond,
-    
-    # # Stochasticity
-    # 'tau_noise': 10*msecond,
-    # 'vnoise_std': 0.5*mV,
-
-    # Layout
-    'r_dish': 4*mm,
-    'weight_distribution': 'singular',
-    
-    # Connectivity: Inh
-    'r_inh': 1*mm,
-    'outdeg_inh': 50,
-    'w_inh_mean': 1,
-
-    # Connectivity: Exc
-    'r_exc': 2*mm,
-    'outdeg_exc': 50,
-    'w_exc_mean': 1,
-
-    # Stimulus
-    'N_stimuli': 5,
-    'stim_distribution_radius': 2.5*mm,
-    'neurons_per_stim': 10,
-    'input_strength': 100,
-
-    # Paradigm
-    'settling_period': 1*second,
-    'sequence_length': 5,
-    'sequence_count': 100,
-    'fully_random_msc': True,
-    'fully_random_oddball': True,
-    'ISI': 100*ms
-}
-
-
-N_networks = 30
-N_templates = 5
-ISIs = (100, 500, 1000, 2000)
-start_at = dict(net=0, isi=100, STD=0, TA=0, templ=0, newnet=True)
-fbase = '/data/felix/culture/isi3_'
-# fbase = 'data/isi2_'
-fname = fbase + 'net{net}_isi{isi}_STD{STD}_TA{TA}_templ{templ}.{type_suffix}.h5'
-netfile = fbase + 'net{net}.h5'
-
-try:
-    dd.io.save(f'{fbase}meta.h5', {k:v for k,v in locals()
-        if k in ('params', 'N_networks', 'N_templates', 'ISIs')})
-except:
-    pass
-
-Xstim, Ystim = spatial.create_stimulus_locations(params)
-stimuli = {key: j for j, key in enumerate('ABCDE')}
-pairings=(('A','B'), ('C','E'))
+Xstim, Ystim = spatial.create_stimulus_locations(cfg.params)
 
 # Set up input templates
-X, Y, W, D = spatial.create_weights(params, rng)
-Net = model.create_network(X, Y, Xstim, Ystim, W, D, params, reset_dt=inputs.get_episode_duration(params))
-templates = [readout.setup_run(Net, params, rng, stimuli, pairings) for _ in range(N_templates)]
+X, Y, W, D = spatial.create_weights(cfg.params, rng)
+Net = model.create_network(X, Y, Xstim, Ystim, W, D, cfg.params, reset_dt=inputs.get_episode_duration(cfg.params))
+templates = [readout.setup_run(Net, cfg.params, rng, cfg.stimuli, cfg.pairings) for _ in range(cfg.N_templates)]
 
 for templ, template in enumerate(templates):
-    if templ < start_at.get('templ', 0):
+    if templ < cfg.start_at.get('templ', 0):
         continue
     else:
-        start_at.pop('templ')
-    for net in range(N_networks):
-        if net < start_at.get('net', 0):
+        cfg.start_at.pop('templ')
+    for net in range(cfg.N_networks):
+        if net < cfg.start_at.get('net', 0):
             continue
         else:
-            start_at.pop('net')
-        if templ == 0 and start_at.pop('newnet', True):
-            X, Y, W, D = spatial.create_weights(params, rng)
+            cfg.start_at.pop('net')
+        if templ == 0 and cfg.start_at.pop('newnet', True):
+            X, Y, W, D = spatial.create_weights(cfg.params, rng)
             try:
-                dd.io.save(netfile.format(net=net), dict(X=X, Y=Y, W=W, D=D))
+                dd.io.save(cfg.netfile.format(net=net), dict(X=X, Y=Y, W=W, D=D))
             except Exception as e:
                 print(e)
         else:
-            res = dd.io.load(netfile.format(net=net))
+            res = dd.io.load(cfg.netfile.format(net=net))
             X, Y, W, D = res['X']*meter, res['Y']*meter, res['W'], res['D']
-        for STD, tau_rec_ in enumerate((0*ms, params['tau_rec'])):
-            for TA, th_ampl_ in enumerate((0*mV, params['th_ampl'])):
-                if STD < start_at.get('STD', 0) or TA < start_at.get('TA', 0):
+        for STD, tau_rec_ in enumerate((0*ms, cfg.params['tau_rec'])):
+            for TA, th_ampl_ in enumerate((0*mV, cfg.params['th_ampl'])):
+                if STD < cfg.start_at.get('STD', 0) or TA < cfg.start_at.get('TA', 0):
                     continue
                 else:
-                    start_at.pop('STD')
-                    start_at.pop('TA')
+                    cfg.start_at.pop('STD')
+                    cfg.start_at.pop('TA')
                 Tstart = time.time()
-                for iISI, isi in enumerate(ISIs):
-                    if isi < start_at.get('isi', ISIs[0]):
+                for iISI, isi in enumerate(cfg.cfg.ISIs):
+                    if isi < cfg.start_at.get('isi', cfg.ISIs[0]):
                         continue
                     else:
-                        start_at.pop('isi')
+                        cfg.start_at.pop('isi')
                     device.reinit()
                     device.activate(**device_args)
                     
-                    mod_params = {**params, 'ISI': isi*ms, 'tau_rec': tau_rec_, 'th_ampl': th_ampl_}
-                    Net = model.create_network(
-                        X, Y, Xstim, Ystim, W, D, mod_params,
-                        reset_dt=inputs.get_episode_duration(mod_params),
-                        state_dt=params['dt'], state_vars=['v'] + (['th_adapt'] if TA else []))
+                    mod_params = {**cfg.params, 'ISI': isi*ms, 'tau_rec': tau_rec_, 'th_ampl': th_ampl_}
+                    if templ == 0:
+                        Net = model.create_network(
+                            X, Y, Xstim, Ystim, W, D, mod_params,
+                            reset_dt=inputs.get_episode_duration(mod_params),
+                            state_dt=cfg.params['dt'], state_vars=['v'] + (['th_adapt'] if TA else []))
+                    else:
+                        Net = model.create_network(
+                            X, Y, Xstim, Ystim, W, D, mod_params,
+                            reset_dt=inputs.get_episode_duration(mod_params))
                     
                     rundata = readout.repeat_run(Net, mod_params, template)
-                    rundata['params'] = mod_params
+                    rundata['cfg.params'] = mod_params
                     Net.run(rundata['runtime'])
-                    readout.get_results(Net, mod_params, rundata, compress=True, tmax=ISIs[0]*ms)
+                    readout.get_results(Net, mod_params, rundata, compress=True, tmax=cfg.ISIs[0]*ms)
 
-                    if STD:
+                    if STD and templ==0:
                         surrogate = {k: {'t': Net[f'SpikeMon_{k}'].t[:], 'i': Net[f'SpikeMon_{k}'].i[:]} for k in ('Exc', 'Inh')}
 
                         device.reinit()
@@ -170,12 +89,12 @@ for templ, template in enumerate(templates):
                         Net = model.create_network(
                             X, Y, Xstim, Ystim, W, D, mod_params_U,
                             reset_dt=inputs.get_episode_duration(mod_params_U),
-                            state_dt=params['dt'], state_vars=['v'],
+                            state_dt=cfg.params['dt'], state_vars=['v'],
                             surrogate=surrogate, suffix='_surrogate')
                         
                         rundata_U = readout.repeat_run(Net, mod_params_U, template)
                         Net.run(rundata_U['runtime'])
-                        readout.get_results(Net, mod_params_U, rundata_U, compress=True, tmax=ISIs[0]*ms)
+                        readout.get_results(Net, mod_params_U, rundata_U, compress=True, tmax=cfg.ISIs[0]*ms)
                         for V_pair, U_pair in zip(rundata['dynamics'], rundata_U['dynamics']):
                             for S in V_pair.keys():
                                 for tp in V_pair[S].keys():
@@ -190,8 +109,7 @@ for templ, template in enumerate(templates):
                                 if not TA:
                                     rt['th_adapt'] = zeros_like(rt['v'])*volt
                     try:
-                        dd.io.save(fname.format(**locals(), type_suffix='spikes'), {k:v for k,v in rundata.items() if k != 'dynamics'})
-                        dd.io.save(fname.format(**locals(), type_suffix='dynamics'), rundata)
+                        dd.io.save(cfg.fname.format(**locals()), rundata)
                     except Exception as e:
                         print(e)
                     brian_cleanup(working_dir)
