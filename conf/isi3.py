540cea2e14a4991392c9002f936d2b968c8ab8a8
Felix B. Kern <kernfel+github@gmail.com>
2022-07-14 10:45:47 +0900

    Added digest-related entries to isi3 config

--- conf/isi3.py
+++ conf/isi3.py
@@ -9,6 +9,9 @@ ISIs = (100, 500, 1000, 2000)
 fbase = '/data/felix/culture/isi3_'
 fname = fbase + 'net{net}_isi{isi}_STD{STD}_TA{TA}_templ{templ}.h5'
 netfile = fbase + 'net{net}.h5'
+digestfile = fbase + '{kind}.h5'
 
 stimuli = {key: j for j, key in enumerate('ABCDE')}
 pairings=(('A','B'), ('C','E'))
+
+contrib_cutoff = .8
