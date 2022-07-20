2dc8422c642a9e59daa8a215780bd62c5dbc80f7
Felix B. Kern <kernfel+github@gmail.com>
2022-07-20 10:56:45 +0900

    Run isi4 on brutus

---
+++ conf/isi4_brutus.py
@@ -0,0 +1,7 @@
+from .isi4 import *
+
+fbase_brutus = '/data/felix/culture/isi4_'
+fname = fname.replace(fbase, fbase_brutus)
+netfile = netfile.replace(fbase, fbase_brutus)
+digestfile = digestfile.replace(fbase, fbase_brutus)
+fbase = fbase_brutus
