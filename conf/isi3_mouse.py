95cd0c7c340d22c033f3beafac1d671e15ed34db
Felix B. Kern <kernfel+github@gmail.com>
2022-07-27 17:59:59 +0900

    Explore DDI & spike counts in isi3

---
+++ conf/isi3_mouse.py
@@ -0,0 +1,7 @@
+from .isi3 import *
+
+fbase_mouse = 'data/isi3_'
+fname = fname.replace(fbase, fbase_mouse)
+netfile = netfile.replace(fbase, fbase_mouse)
+digestfile = digestfile.replace(fbase, fbase_mouse)
+fbase = fbase_mouse
