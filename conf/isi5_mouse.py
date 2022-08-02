5818b04af5fbcb671c687f97b8a14450a811f597
Felix B. Kern <kernfel+github@gmail.com>
2022-08-02 10:19:15 +0900

    Added memory duration NB

---
+++ conf/isi5_mouse.py
@@ -0,0 +1,7 @@
+from .isi5 import *
+
+fbase_mouse = 'data/isi5_'
+fname = fname.replace(fbase, fbase_mouse)
+netfile = netfile.replace(fbase, fbase_mouse)
+digestfile = digestfile.replace(fbase, fbase_mouse)
+fbase = fbase_mouse
